5e902dadc70ea52fdb57db046cb57d59e167805b
BOlimpio <b.olimpio@outlook.com>
2023-08-16 22:33:35 -0300

    add v1 portal iac backend in commom python files

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# portal-iac-backend
-ToBe defined
+# Portal Backend Integration Script [IN PROGRESS :hourglass:]
+
+This Python script facilitates the integration between the Portal IAC frontend and GitHub, allowing data retrieval, parsing, and file downloading from specific repositories. The script is designed to provide dynamic content display on the portal.
+
+## Features
+
+- Fetches GitHub repositories with the prefix "portal-"
+- Retrieves specific information for all modules in found repositories
+- Reads module data from the "module-data.conf" file in each repository
+- Checks for new commits in repositories
+- Downloads files from the "how-to-use" folder in repositories
+
+## Prerequisites
+
+- Python 3.x
+- Required Python packages: `github`, `requests`
+
+## Setup
+
+1. Clone or download the script to your local machine.
+2. Create a `config.ini` file in the same directory with the following content:
+
+    ```ini
+    [github]
+    token = YOUR_GITHUB_TOKEN
+    ```
+
+3. Replace YOUR_GITHUB_TOKEN with your GitHub API token.
+
+## Usage
+
+1. Open a terminal and navigate to the script's directory.
+2. Run the script using the following command:
+    ```
+    python github_integration.py
+    ```
+3. The script will fetch repository data, read module information, and download relevant files.
+
+## Notes
+- This script assumes that repositories follow the naming convention **"portal-"**.
+- Ensure that the config.ini file is kept secure and not shared.
+
+## Future Steps :rocket::rocket:
+- As the Portal IAC project evolves, this integration script is planned to be further optimized and transformed into **AWS Lambda functions** for enhanced scalability and efficiency.
---
+++ github_integration.py
@@ -0,0 +1,84 @@
+import os
+import json
+import requests
+from github import Github
+import configparser
+
+class PortalBackend:
+    def __init__(self, github_token):
+        self.github = Github(github_token)
+        self.module_repositories = self.get_module_repositories()
+
+    # Fetch GitHub Repositories with Prefix "portal-"
+    def get_module_repositories(self):
+        module_repositories = []
+        user = self.github.get_user()
+        for repo in user.get_repos():
+            if repo.name.startswith("portal-"):
+                module_repositories.append(repo)
+        return module_repositories
+    
+     # Fetches and returns specific information for all modules in found repositories
+    def get_module_data(self):
+        module_data_list = []
+        for repo in self.module_repositories:
+            module_data = self.read_module_data(repo)
+            module_data_list.append(module_data)
+        return module_data_list
+    
+    # Read module-data.conf file from each repository
+    def read_module_data(self, repo):
+        module_data = {}
+        try:
+            content = repo.get_contents("module-data.conf")
+            data = content.decoded_content.decode("utf-8")
+            config = configparser.ConfigParser()
+            config.read_string(data)
+            
+            if "module-info" in config:
+                module_data["title"] = config.get("module-info", "Titulo")
+                module_data["description"] = config.get("module-info", "Descrição")
+                module_data["status"] = config.get("module-info", "Status")
+                module_data["cloud_provider"] = config.get("module-info", "Cloud Provider")
+                module_data["version"] = config.get("module-info", "Versão")
+                module_data["developers"] = config.get("module-info", "Desenvolvedores").split(";")
+        except Exception as e:
+            print(f"Error reading module data for repo {repo.name}: {e}")
+        return module_data
+    
+    # Check for New Commits
+    def has_new_commits(self, repo, last_commit_count):
+        current_commit_count = repo.get_commits().totalCount
+        return current_commit_count > last_commit_count
+    
+    # Downloading files from the "how-to-use" folder
+    def download_how_to_use_files(self, repo, destination_folder):
+        try:
+            contents = repo.get_contents("how-to-use")
+            for content in contents:
+                if content.type == "file":
+                    url = content.download_url
+                    file_name = os.path.join(destination_folder, content.name)
+                    response = requests.get(url)
+                    if response.status_code == 200:
+                        with open(file_name, "wb") as file:
+                            file.write(response.content)
+                        print(f"Downloaded: {content.name}")
+        except Exception as e:
+            print(f"Error downloading files from repo {repo.name}: {e}")
+
+
+# Main code
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    github_token = config.get("github", "token")
+    
+    backend = PortalBackend(github_token)
+    
+    module_data_list = backend.get_module_data()
+    print(json.dumps(module_data_list, indent=2))
+    
+    for repo in backend.module_repositories:
+        if backend.has_new_commits(repo):
+            backend.download_how_to_use_files(repo)
